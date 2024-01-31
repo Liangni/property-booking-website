c87fbd0e3a7e7c9fa80d336f84956f9bca118709
Penny Pan <pennypan@Pennys-MacBook-Air.local>
2024-01-31 16:46:15 +0800

    initial commit

---
+++ src/main/java/com/penny/PropertyBookingWebiteApplication.java
@@ -0,0 +1,13 @@
+package com.penny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyBookingWebiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyBookingWebiteApplication.class, args);
+	}
+
+}
