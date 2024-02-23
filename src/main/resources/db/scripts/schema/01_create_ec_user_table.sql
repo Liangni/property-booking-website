b271562c2a4634eb967f3a3a69ca0fd2b71d932f
Penny Pan <pennypan@Pennys-MacBook-Air.local>
2024-02-23 16:42:29 +0800

    add prefix to fields of ec_user table

--- src/main/resources/db/scripts/schema/01_create_ec_user_table.sql
+++ src/main/resources/db/scripts/schema/01_create_ec_user_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS ec_user
 (
     ec_user_id BIGSERIAL PRIMARY KEY,
     ec_user_name VARCHAR(30) NOT NULL,
-    email VARCHAR(30) NOT NULL,
-    hashed_password VARCHAR(100) NOT NULL,
+    ec_user_email VARCHAR(30) NOT NULL,
+    ec_user_hashed_password VARCHAR(100) NOT NULL,
     ec_user_introduction TEXT
 );
