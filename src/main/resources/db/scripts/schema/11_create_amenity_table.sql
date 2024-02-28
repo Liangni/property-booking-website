56a623bc6def2f0b94a6f4addf844845de5828bf
Penny Pan <pennypan@Pennys-MacBook-Air.local>
2024-02-28 19:03:47 +0800

    為 amenity_table 補上 primary key

--- src/main/resources/db/scripts/schema/11_create_amenity_table.sql
+++ src/main/resources/db/scripts/schema/11_create_amenity_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS amenity
 (
-    amenity_id BIGSERIAL NOT NULL,
+    amenity_id BIGSERIAL PRIMARY KEY NOT NULL,
     amenity_name VARCHAR(150),
     amenity_type_id BIGINT NOT NULL
 )
