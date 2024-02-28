19d66a0c4ea9e026d3f1637eab8758e577a3e162
Penny Pan <pennypan@Pennys-MacBook-Air.local>
2024-02-28 12:27:59 +0800

    為 DistrictVoMapperTest 補上測試資料

--- src/test/java/com/penny/dao/DistrictVoMapperTest.java
+++ src/test/java/com/penny/dao/DistrictVoMapperTest.java
@@ -1,6 +1,10 @@
 package com.penny.dao;
 
+import com.penny.dao.base.AdministrativeAreaBaseVoMapper;
+import com.penny.dao.base.DistrictBaseVoMapper;
 import com.penny.vo.DistrictVo;
+import com.penny.vo.base.AdministrativeAreaBaseVo;
+import com.penny.vo.base.DistrictBaseVo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -19,20 +23,48 @@ import java.util.Map;
 public class DistrictVoMapperTest {
     @Autowired
     private DistrictVoMapper districtVoMapper;
+    @Autowired
+    private DistrictBaseVoMapper districtBaseVoMapper;
+    @Autowired
+    private AdministrativeAreaBaseVoMapper administrativeAreaBaseVoMapper;
+
+    String testKeyword = "test";
+
+    @BeforeEach
+    void setup(){
+        // 新增行政區劃分層級資料
+        AdministrativeAreaBaseVo administrativeArea = AdministrativeAreaBaseVo.builder()
+                .administrativeAreaName("test name")
+                .administrativeAreaLevel(100L)
+                .build();
+        administrativeAreaBaseVoMapper.insertSelective(administrativeArea);
+
+        // 獲取新行政區劃分層級資料 id
+        long administrativeAreaId =(long) administrativeArea.getAdministrativeAreaId();
+
+        // 以行政區劃分層級資料 id 新增地區資料
+        DistrictBaseVo district = DistrictVo.builder()
+                .districtName("test name")
+                .administrativeAreaId(administrativeAreaId) // 帶入行政區劃分層級資料 id
+                .build();
+
+        districtBaseVoMapper.insertSelective(district);
+    }
+
 
     @Test
     @DisplayName("用 district_name 關鍵字搜尋地區")
     void selectByNameKeywordTest(){
-        List<DistrictVo> districtVoList = districtVoMapper.selectByNameKeyword("縣", 0, 5);
+        List<DistrictVo> districtVoList = districtVoMapper.selectByNameKeyword(testKeyword, 0, 5);
 
         Assertions.assertNotNull(districtVoList);
-        Assertions.assertEquals(districtVoList.size(), 5);
+        Assertions.assertNotEquals(0, districtVoList.size());
     }
 
     @Test
     @DisplayName("計算用 district_name 關鍵字搜尋地區所找到的資料筆數")
     void selectAndCountByNameKeywordTest(){
-        Integer count = districtVoMapper.countSelectByNameKeyword("縣");
+        Integer count = districtVoMapper.countSelectByNameKeyword(testKeyword);
 
         Assertions.assertNotNull(count);
         Assertions.assertNotEquals(0, count);
