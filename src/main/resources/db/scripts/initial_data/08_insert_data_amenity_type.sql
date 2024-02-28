f14b955fb14547a83c6283e8362c783fcf226bf4
Penny Pan <pennypan@Pennys-MacBook-Air.local>
2024-02-28 17:29:10 +0800

    增加 amenity_type_table sql 檔案

---
+++ src/main/resources/db/scripts/initial_data/08_insert_data_amenity_type.sql
@@ -0,0 +1,9 @@
+INSERT INTO amenity_type (amenity_type_name) VALUES
+('基本設備'),
+('衛浴'),
+('臥室和洗衣'),
+('娛樂'),
+('親子'),
+('網路和辦公'),
+('廚房和餐飲'),
+('位置特色');
---
+++ src/main/resources/db/scripts/schema/10_create_amenity_type_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS amenity_type
+(
+    amenity_type_id BIGSERIAL PRIMARY KEY,
+    amenity_type_name VARCHAR(150) NOT NULL
+)
