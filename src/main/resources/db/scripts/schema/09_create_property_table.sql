0157fd11f1a90b062e29ddd8315eb2b7e7ba0d74
Penny Pan <pennypan@Pennys-MacBook-Air.local>
2024-02-29 18:00:45 +0800

    在 property 表加上 property_title 屬性

--- src/main/resources/db/scripts/schema/09_create_property_table.sql
+++ src/main/resources/db/scripts/schema/09_create_property_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS property
 (
-    property_id BIGSERIAL PRIMARY KEY,
+    property_id BIGSERIAL PRIMARY KEY NOT NULL,
+    property_title VARCHAR(150) NOT NULL,
     property_description TEXT,
     max_num_of_guests INT,
     num_of_bedrooms INT,
@@ -13,7 +14,7 @@ CREATE TABLE IF NOT EXISTS property
     property_group_type_id BIGINT,
     property_share_type_id BIGINT,
     address_id BIGINT,
-    host_id BIGINT,
+    host_id BIGINT NOT NULL,
     FOREIGN KEY (property_group_type_id) REFERENCES property_group_type(property_group_type_id),
     FOREIGN KEY (property_share_type_id) REFERENCES property_share_type(property_share_type_id),
     FOREIGN KEY (address_id) REFERENCES address(address_id),
