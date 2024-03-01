d5b1abf1cb48cf43afa673a053cae48c945702f9
Penny Pan <pennypan@Pennys-MacBook-Air.local>
2024-03-01 17:36:54 +0800

    增加 address table 屬性 admin_area_level_1_district_id, admin_area_level_2_district_id, admin_area_level_3_district_id

--- src/main/resources/db/scripts/example_data/02_insert_example_address.sql
+++ src/main/resources/db/scripts/example_data/02_insert_example_address.sql
@@ -1,3 +1,3 @@
-INSERT INTO address(apartment_floor, street, district_id) VALUES
-('test floor 1', 'test street 1', 180),
-('test floor 2', 'test street 2', 150);
+INSERT INTO address(apartment_floor, street, admin_area_level_1_district_id, admin_area_level_2_district_id, admin_area_level_3_district_id) VALUES
+('test floor 1', 'test street 1', null, 165,181),
+('test floor 2', 'test street 2', 6, null, 127);
--- src/main/resources/db/scripts/schema/08_create_address_table.sql
+++ src/main/resources/db/scripts/schema/08_create_address_table.sql
@@ -3,6 +3,10 @@ CREATE TABLE IF NOT EXISTS address
     address_id BIGSERIAL PRIMARY KEY,
     apartment_floor VARCHAR(150),
     street VARCHAR(150) NOT NULL,
-    district_id BIGINT NOT NULL,
-    FOREIGN KEY (district_id) REFERENCES district(district_id)
+    admin_area_level_1_district_id BIGINT,
+    admin_area_level_2_district_id BIGINT,
+    admin_area_level_3_district_id BIGINT,
+    FOREIGN KEY (admin_area_level_1_district_id) REFERENCES district(district_id),
+    FOREIGN KEY (admin_area_level_2_district_id) REFERENCES district(district_id),
+    FOREIGN KEY (admin_area_level_3_district_id) REFERENCES district(district_id)
 )
