3a01f33af7b96b1c74fd0812b9870e5eedaaf998
Penny Pan <pennypan@Pennys-MacBook-Air.local>
2024-03-01 17:37:46 +0800

    新增 property vo 和 mapper

---
+++ src/main/java/com/penny/dao/PropertyVoMapper.java
@@ -0,0 +1,8 @@
+package com.penny.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PropertyVoMapper {
+
+}
---
+++ src/main/java/com/penny/vo/PropertyVo.java
@@ -0,0 +1,12 @@
+package com.penny.vo;
+
+import com.penny.vo.base.PropertyBaseVo;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class PropertyVo extends PropertyBaseVo {
+}
