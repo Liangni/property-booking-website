5f8de86ec0cdf22b75c30f69cdb5360903e36e72
Penny Pan <pennypan@Pennys-MacBook-Air.local>
2024-03-04 13:32:29 +0800

    修改 property 和 booking_availability 的 example data sql

--- src/main/resources/db/scripts/example_data/03_insert_example_property.sql
+++ src/main/resources/db/scripts/example_data/03_insert_example_property.sql
@@ -1,3 +1,4 @@
 INSERT INTO property(property_title, max_num_of_guests, address_id, host_id) VALUES
 ('test property 1', 2, 1, 1),
-('test property 2', 3, 2, 1);
+('test property 2', 2, 1, 1),
+('test property 3', 2, 2, 1);
