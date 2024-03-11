684eae6c3c2eb438b9956b4126b76b1235dca227
Penny Pan <light0104@gmail.com>
2024-03-11 17:07:39 +0800

    移除 InitialPageService 返回的房源物件內，值為 Null 的屬性

--- src/main/java/com/penny/service/InitialPageService.java
+++ src/main/java/com/penny/service/InitialPageService.java
@@ -9,10 +9,7 @@ import com.penny.vo.PropertyVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class InitialPageService {
@@ -68,24 +65,48 @@ public class InitialPageService {
         // 根據房源查詢參數查詢房源列表
         List<PropertyVo> propertyVoList = propertyVoMapper.listByNumOfAvailableDays(param);
 
+
         // 計算圖片詳細資訊的偏移量並為每個房源設置圖片詳細資訊列表和評論數
         int pictureDtOffset = calculateOffset(DEFAULT_PICTURE_DT_PAGE, DEFAULT_PICTURE_DT_LIMIT);
+        List<Map<String, Object>> leanProeprtyList= new ArrayList<>();
+
         for(PropertyVo propertyVo: propertyVoList) {
             Long propertyId = propertyVo.getPropertyId();
+            Long priceOnWeekdays = propertyVo.getPriceOnWeekdays();
+            String adminAreaLevel1DistrictName = propertyVo.getAdminAreaLevel1DistrictName();
+            String adminAreaLevel2DistrictName = propertyVo.getAdminAreaLevel2DistrictName();
+            String adminAreaLevel3DistrictName = propertyVo.getAdminAreaLevel3DistrictName();
+            Date startAvailableDate = propertyVo.getStartAvailableDate();
+            Date endAvailableDate = propertyVo.getEndAvailableDate();
+            Double averageRating = propertyVo.getAverageRating();
+
+            // 創建一個新的房源 Map，存放房源的資訊
+            Map<String, Object> propertyMap = new HashMap<>();
+            propertyMap.put("propertyId", propertyId);
+            propertyMap.put("priceOnWeekdays", priceOnWeekdays);
+            propertyMap.put("parentDistrictName", Optional.ofNullable(adminAreaLevel1DistrictName)
+                    .orElse(adminAreaLevel2DistrictName)
+            );
+            propertyMap.put("childDistrictName", adminAreaLevel3DistrictName);
+            propertyMap.put("startAvailableDate", startAvailableDate);
+            propertyMap.put("endAvailableDate", endAvailableDate);
+            propertyMap.put("averageRating", averageRating);
 
             // 查詢房源的圖片詳細資訊列表
             List<PictureDtVo> pictureDtVoList = pictureDtVoMapper.listByPropertyId(propertyId, DEFAULT_PICTURE_DT_SIZE, pictureDtOffset, DEFAULT_PICTURE_DT_LIMIT);
-            propertyVo.setPictureDtVoList(pictureDtVoList);
+            propertyMap.put("pictureDtList", pictureDtVoList);
 
             // 查詢房源的評論數
             Map<String, Object> reviewCountMap = propertyReviewVoMapper.countByPropertyId(propertyId);
-            propertyVo.setReviewCount((long)reviewCountMap.get("count"));
+            propertyMap.put("reviewCount", reviewCountMap.get("count"));
+
+            // 將房源 Map 添加到 leanPropertyList 中
+            leanProeprtyList.add(propertyMap);
         }
 
 
-
         Map<String, Object> resultMap = new HashMap<>();
-        resultMap.put("result", propertyVoList);
+        resultMap.put("result", leanProeprtyList);
         return resultMap;
     }
 
