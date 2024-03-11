617546b46e9171a9da44894b1e4cf58171f7fecb
Penny Pan <light0104@gmail.com>
2024-03-11 13:51:02 +0800

    增加 property_picture example data

--- src/main/resources/db/scripts/example_data/05_insert_example_picture.sql
+++ src/main/resources/db/scripts/example_data/05_insert_example_picture.sql
@@ -1,11 +1,11 @@
 INSERT INTO picture(picture_url) VALUES
-('www.fake.image1.jpg'),
-('www.fake.image2.jpg'),
-('www.fake.image3.jpg'),
-('www.fake.image4.jpg'),
-('www.fake.image5.jpg'),
-('www.fake.image6.jpg'),
-('www.fake.image7.jpg'),
-('www.fake.image8.jpg'),
-('www.fake.image9.jpg');
+('www.fake.image11.jpg'),
+('www.fake.image12.jpg'),
+('www.fake.image13.jpg'),
+('www.fake.image21.jpg'),
+('www.fake.image22.jpg'),
+('www.fake.image23.jpg'),
+('www.fake.image31.jpg'),
+('www.fake.image32.jpg'),
+('www.fake.image33.jpg');
 
--- src/main/resources/db/scripts/example_data/06_insert_example_picture_dt.sql
+++ src/main/resources/db/scripts/example_data/06_insert_example_picture_dt.sql
@@ -1,80 +1,80 @@
--- 為圖片1在 picture_dt 表插入詳細資訊（大、中、小）
+-- 為圖片11在 picture_dt 表插入詳細資訊（大、中、小）
 INSERT INTO picture_dt (picture_id, picture_dt_size, picture_dt_url)
 SELECT picture_id, -- 從 picture 表中選擇 picture_id 欄位
        size, -- 選擇由 VALUES 子句創建的 sizes 臨時表中的size列
-       CONCAT('www.fake.image1.', size, '.jpg') -- 將圖片URL組合成完整的URL
+       CONCAT('www.fake.image11.', size, '.jpg') -- 將圖片URL組合成完整的URL
 FROM (VALUES ('large'), ('medium'), ('small')) AS sizes(size) -- 創建一個包含尺寸的臨時表sizes
 CROSS JOIN picture -- 與 picture 表交叉組合以獲取圖片 picture_id
-WHERE picture_url = 'www.fake.image1.jpg';
+WHERE picture_url = 'www.fake.image11.jpg';
 
--- 為圖片2在 picture_dt 表插入詳細資訊（大、中、小）
+-- 為圖片12在 picture_dt 表插入詳細資訊（大、中、小）
 INSERT INTO picture_dt (picture_id, picture_dt_size, picture_dt_url)
 SELECT picture_id,
        size,
-       CONCAT('www.fake.image2.', size, '.jpg')
+       CONCAT('www.fake.image12.', size, '.jpg')
 FROM (VALUES ('large'), ('medium'), ('small')) AS sizes(size)
 CROSS JOIN picture
-WHERE picture_url = 'www.fake.image2.jpg';
+WHERE picture_url = 'www.fake.image12.jpg';
 
--- 為圖片3在 picture_dt 表插入詳細資訊（大、中、小）
+-- 為圖片13在 picture_dt 表插入詳細資訊（大、中、小）
 INSERT INTO picture_dt (picture_id, picture_dt_size, picture_dt_url)
 SELECT picture_id,
        size,
-       CONCAT('www.fake.image3.', size, '.jpg')
+       CONCAT('www.fake.image13.', size, '.jpg')
 FROM (VALUES ('large'), ('medium'), ('small')) AS sizes(size)
          CROSS JOIN picture
-WHERE picture_url = 'www.fake.image3.jpg';
+WHERE picture_url = 'www.fake.image13.jpg';
 
--- 為圖片4在 picture_dt 表插入詳細資訊（大、中、小）
+-- 為圖片21在 picture_dt 表插入詳細資訊（大、中、小）
 INSERT INTO picture_dt (picture_id, picture_dt_size, picture_dt_url)
 SELECT picture_id,
        size,
-       CONCAT('www.fake.image4.', size, '.jpg')
+       CONCAT('www.fake.image21.', size, '.jpg')
 FROM (VALUES ('large'), ('medium'), ('small')) AS sizes(size)
          CROSS JOIN picture
-WHERE picture_url = 'www.fake.image4.jpg';
+WHERE picture_url = 'www.fake.image21.jpg';
 
--- 為圖片5在 picture_dt 表插入詳細資訊（大、中、小）
+-- 為圖片22在 picture_dt 表插入詳細資訊（大、中、小）
 INSERT INTO picture_dt (picture_id, picture_dt_size, picture_dt_url)
 SELECT picture_id,
        size,
-       CONCAT('www.fake.image5.', size, '.jpg')
+       CONCAT('www.fake.image22.', size, '.jpg')
 FROM (VALUES ('large'), ('medium'), ('small')) AS sizes(size)
          CROSS JOIN picture
-WHERE picture_url = 'www.fake.image5.jpg';
+WHERE picture_url = 'www.fake.image22.jpg';
 
--- 為圖片6在 picture_dt 表插入詳細資訊（大、中、小）
+-- 為圖片23在 picture_dt 表插入詳細資訊（大、中、小）
 INSERT INTO picture_dt (picture_id, picture_dt_size, picture_dt_url)
 SELECT picture_id,
        size,
-       CONCAT('www.fake.image6.', size, '.jpg')
+       CONCAT('www.fake.image23.', size, '.jpg')
 FROM (VALUES ('large'), ('medium'), ('small')) AS sizes(size)
          CROSS JOIN picture
-WHERE picture_url = 'www.fake.image6.jpg';
+WHERE picture_url = 'www.fake.image23.jpg';
 
--- 為圖片7在 picture_dt 表插入詳細資訊（大、中、小）
+-- 為圖片31在 picture_dt 表插入詳細資訊（大、中、小）
 INSERT INTO picture_dt (picture_id, picture_dt_size, picture_dt_url)
 SELECT picture_id,
        size,
-       CONCAT('www.fake.image7.', size, '.jpg')
+       CONCAT('www.fake.image31.', size, '.jpg')
 FROM (VALUES ('large'), ('medium'), ('small')) AS sizes(size)
          CROSS JOIN picture
-WHERE picture_url = 'www.fake.image7.jpg';
+WHERE picture_url = 'www.fake.image31.jpg';
 
--- 為圖片8在 picture_dt 表插入詳細資訊（大、中、小）
+-- 為圖片32在 picture_dt 表插入詳細資訊（大、中、小）
 INSERT INTO picture_dt (picture_id, picture_dt_size, picture_dt_url)
 SELECT picture_id,
        size,
-       CONCAT('www.fake.image8.', size, '.jpg')
+       CONCAT('www.fake.image32.', size, '.jpg')
 FROM (VALUES ('large'), ('medium'), ('small')) AS sizes(size)
          CROSS JOIN picture
-WHERE picture_url = 'www.fake.image8.jpg';
+WHERE picture_url = 'www.fake.image32.jpg';
 
--- 為圖片9在 picture_dt 表插入詳細資訊（大、中、小）
+-- 為圖片33在 picture_dt 表插入詳細資訊（大、中、小）
 INSERT INTO picture_dt (picture_id, picture_dt_size, picture_dt_url)
 SELECT picture_id,
        size,
-       CONCAT('www.fake.image9.', size, '.jpg')
+       CONCAT('www.fake.image33.', size, '.jpg')
 FROM (VALUES ('large'), ('medium'), ('small')) AS sizes(size)
          CROSS JOIN picture
-WHERE picture_url = 'www.fake.image9.jpg';
+WHERE picture_url = 'www.fake.image33.jpg';
