55f5daa7866e6514406db3758f352bdcb7f4bf30
Penny Pan <light0104@gmail.com>
2024-04-08 12:51:23 +0800

    移動 s3Service 至 s3 package

--- src/main/java/com/penny/s3/S3Service.java
+++ src/main/java/com/penny/s3/S3Service.java
@@ -1,4 +1,4 @@
-package com.penny.service;
+package com.penny.s3;
 
 import com.amazonaws.HttpMethod;
 import com.amazonaws.services.s3.AmazonS3;
--- src/main/java/com/penny/service/PropertyService.java
+++ src/main/java/com/penny/service/PropertyService.java
@@ -14,6 +14,7 @@ import com.penny.exception.ResourceNotFoundException;
 import com.penny.request.property.PropertySearchParam;
 import com.penny.request.property.PropertyUploadImageRequest;
 import com.penny.s3.S3Buckets;
+import com.penny.s3.S3Service;
 import com.penny.vo.PictureDtVo;
 import com.penny.vo.PropertyPictureVo;
 import com.penny.vo.PropertyVo;
@@ -22,8 +23,6 @@ import com.penny.vo.base.PictureDtBaseVo;
 import com.penny.vo.base.PropertyBaseVo;
 import com.penny.vo.base.PropertyPictureBaseVo;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
