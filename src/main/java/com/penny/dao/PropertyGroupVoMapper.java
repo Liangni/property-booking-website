d1f37192ef2745870dc69ede2dfd4714e297beda
Penny Pan <light0104@gmail.com>
2024-04-09 15:17:57 +0800

    在 PropertyGroupVoMapper 標註 @Mapper

--- src/main/java/com/penny/dao/PropertyGroupVoMapper.java
+++ src/main/java/com/penny/dao/PropertyGroupVoMapper.java
@@ -2,9 +2,11 @@ package com.penny.dao;
 
 import com.penny.vo.PropertyGroupVo;
 import com.penny.vo.PropertyPictureVo;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper
 public interface PropertyGroupVoMapper {
     List<PropertyGroupVo> listAll();
 }
