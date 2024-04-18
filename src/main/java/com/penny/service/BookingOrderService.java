3f3d218bea28743775eb9fe8d39096c8e8023e66
Penny Pan <light0104@gmail.com>
2024-04-18 18:00:43 +0800

    在 BookingOrderService 的 createBookingOrder 方法增加對日期參數的檢驗

--- src/main/java/com/penny/service/BookingOrderService.java
+++ src/main/java/com/penny/service/BookingOrderService.java
@@ -74,15 +74,29 @@ public class BookingOrderService {
             throw new FieldConflictException("arrivalTime is required");
         }
 
+        // 檢查房源是否為已發布
+        PropertyBaseVo propertyBaseVo = propertyBaseVoMapper.selectByPrimaryKey(propertyId);
+        if(propertyBaseVo == null || !propertyBaseVo.getIsPublished()) {
+            throw new ResourceNotFoundException("property with propertyId %s not found".formatted(propertyId));
+        }
+
         // 檢查日期格式
         if (!dateHelper.isValidDateString(request.getCheckinDate()) || !dateHelper.isValidDateString(request.getCheckoutDate())) {
             throw new FieldConflictException("checkinDate and checkoutDate should be in yyyy-mm-dd format");
         }
 
-        // 檢查房源是否為已發布
-        PropertyBaseVo propertyBaseVo = propertyBaseVoMapper.selectByPrimaryKey(propertyId);
-        if(propertyBaseVo == null || !propertyBaseVo.getIsPublished()) {
-            throw new ResourceNotFoundException("property with propertyId %s not found".formatted(propertyId));
+        // 解析預訂日期
+        LocalDate checkinDate = dateHelper.parseDateString(request.getCheckinDate());
+        LocalDate checkoutDate = dateHelper.parseDateString(request.getCheckoutDate());
+
+        // 檢查 checkin 日期是否早於 checkout 日期
+        if(checkinDate.isAfter(checkoutDate)) {
+            throw new FieldConflictException("checkinDate should be before checkoutDate");
+        }
+
+        // 檢查 checkin 日期是否是未來日期
+        if (checkinDate.isBefore(LocalDate.now())){
+            throw new FieldConflictException("checkinDate should be later than today");
         }
 
         // 獲取預訂訂單基本資訊
@@ -90,12 +104,8 @@ public class BookingOrderService {
         bookingOrderBaseVo.setHostId(propertyBaseVo.getHostId());
         bookingOrderBaseVo.setCustomerId(ecUserService.getLoginUser().getEcUserId());
 
-        // 解析預訂日期
-        LocalDate checkinDate = dateHelper.parseDateString(request.getCheckinDate());
-        LocalDate checkoutDate = dateHelper.parseDateString(request.getCheckoutDate());
-        List<BookingAvailabilityVo> bookingAvailabilityVoList = bookingAvailabilityVoMapper.listByStartAndEndDate(propertyId, checkinDate, checkoutDate);
-
         // 檢查資料庫中的預訂日期是否足夠覆蓋指定日期範圍
+        List<BookingAvailabilityVo> bookingAvailabilityVoList = bookingAvailabilityVoMapper.listByStartAndEndDate(propertyId, checkinDate, checkoutDate);
         if (bookingAvailabilityVoList.size() < countDayDifference(checkinDate, checkoutDate)) {
             // 生成指定日期範圍內的連續日期列表
             List<LocalDate> bookingLocalDateList = generateDateRange(checkinDate, checkoutDate);
@@ -106,16 +116,20 @@ public class BookingOrderService {
         }
 
         int weekendCount = 0;
+        List<LocalDate> bookedDateList = new ArrayList<>();
         for(BookingAvailabilityVo bookingDate : bookingAvailabilityVoList) {
             // 檢查日期可預訂性
             boolean isBooked = bookingDate.getBookingAvailabilityStatus().equals("booked");
-            if (isBooked) {
-                throw new FieldConflictException("%s has been booked".formatted(bookingDate.getBookingAvailabilityDate()));
-            }
+            if (isBooked) bookedDateList.add(bookingDate.getBookingAvailabilityDate());
 
             // 計算假日數量
             LocalDate currentDate = bookingDate.getBookingAvailabilityDate();
             if (dateHelper.isWeekend(currentDate)) weekendCount ++;
+        }
+
+        // 若 checkin 至 checkout 期間有日期是已預訂，則拋出錯誤
+        if (!bookedDateList.isEmpty()) {
+            throw new FieldConflictException("date range %s has been booked".formatted(bookedDateList));
         }
 
         // 依照平日假日價格計算訂房費用
@@ -134,13 +148,16 @@ public class BookingOrderService {
         BigDecimal totalAfterDiscount = BigDecimal.valueOf(Math.ceil(totalBeforeDiscount - discountValue));
         bookingOrderBaseVo.setOrderTotal(totalAfterDiscount);
 
-        // 更新預定日期狀態
+        // 提取 bookingAvailabilityVoList 中每個物件的 bookingAvailabilityId 到新的列表中
         List<Long> bookingAvailabilityIdList = bookingAvailabilityVoList
                 .stream()
                 .map(BookingAvailabilityVo::getBookingAvailabilityId)
                 .collect(Collectors.toList());
+
+        // 更新 bookingAvailabilityIdList 中對應的預定日期的狀態
         int updateCount = bookingAvailabilityVoMapper.setStatusToBookingByPrimaryKeyList(bookingAvailabilityIdList);
 
+        // 檢查更新的數量是否與預期的數量相同，如果不同則拋出異常
         if (updateCount != bookingAvailabilityVoList.size()) {
             throw new FieldConflictException("The specified dates have been booked");
         }
