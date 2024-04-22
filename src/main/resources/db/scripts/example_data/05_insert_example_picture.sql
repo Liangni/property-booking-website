8e25d39f10d4caa43eaa848bb8e30641f9821f2b
Penny Pan <light0104@gmail.com>
2024-04-22 20:55:28 +0800

    修改房源圖片 example_data 的房源連結

--- src/main/resources/db/scripts/example_data/05_insert_example_picture.sql
+++ src/main/resources/db/scripts/example_data/05_insert_example_picture.sql
@@ -1,11 +1,11 @@
 INSERT INTO picture(picture_url) VALUES
-('www.fake.image11.jpg'),
-('www.fake.image12.jpg'),
-('www.fake.image13.jpg'),
-('www.fake.image21.jpg'),
-('www.fake.image22.jpg'),
-('www.fake.image23.jpg'),
-('www.fake.image31.jpg'),
-('www.fake.image32.jpg'),
-('www.fake.image33.jpg');
+('properties/1/size-original/7f3b28ab-a802-43cc-bca6-14b0c6653811.png'),
+('properties/1/size-original/7f3b28ab-a802-43cc-bca6-14b0c6653812.png'),
+('properties/1/size-original/7f3b28ab-a802-43cc-bca6-14b0c6653813.png'),
+('properties/2/size-original/7f3b28ab-a802-43cc-bca6-14b0c6653821.png'),
+('properties/2/size-original/7f3b28ab-a802-43cc-bca6-14b0c6653822.png'),
+('properties/2/size-original/7f3b28ab-a802-43cc-bca6-14b0c6653823.png'),
+('properties/3/size-original/7f3b28ab-a802-43cc-bca6-14b0c6653831.png'),
+('properties/3/size-original/7f3b28ab-a802-43cc-bca6-14b0c6653832.png'),
+('properties/3/size-original/7f3b28ab-a802-43cc-bca6-14b0c6653833.png');
 
--- src/main/resources/db/scripts/example_data/07_insert_example_property_picture.sql
+++ src/main/resources/db/scripts/example_data/07_insert_example_property_picture.sql
@@ -1,23 +1,23 @@
 INSERT INTO property_picture(property_id, picture_id, property_picture_order)
 SELECT pro.property_id, pic.picture_id, subquery.order_num
 FROM (
-         SELECT 'www.fake.image11.jpg' AS picture_url, 1 AS order_num, 'test property 1' AS property_title
+         SELECT 'properties/1/size-original/7f3b28ab-a802-43cc-bca6-14b0c6653811.png' AS picture_url, 1 AS order_num, 'test property 1' AS property_title
          UNION ALL
-         SELECT 'www.fake.image12.jpg', 2, 'test property 1'
+         SELECT 'properties/1/size-original/7f3b28ab-a802-43cc-bca6-14b0c6653812.png', 2, 'test property 1'
          UNION ALL
-         SELECT 'www.fake.image13.jpg', 3, 'test property 1'
+         SELECT 'properties/1/size-original/7f3b28ab-a802-43cc-bca6-14b0c6653813.png', 3, 'test property 1'
          UNION ALL
-         SELECT 'www.fake.image21.jpg', 1, 'test property 2'
+         SELECT 'properties/2/size-original/7f3b28ab-a802-43cc-bca6-14b0c6653821.png', 1, 'test property 2'
          UNION ALL
-         SELECT 'www.fake.image22.jpg', 2, 'test property 2'
+         SELECT 'properties/2/size-original/7f3b28ab-a802-43cc-bca6-14b0c6653822.png', 2, 'test property 2'
          UNION ALL
-         SELECT 'www.fake.image23.jpg', 3, 'test property 2'
+         SELECT 'properties/2/size-original/7f3b28ab-a802-43cc-bca6-14b0c6653823.png', 3, 'test property 2'
          UNION ALL
-         SELECT 'www.fake.image31.jpg', 1, 'test property 3'
+         SELECT 'properties/3/size-original/7f3b28ab-a802-43cc-bca6-14b0c6653831.png', 1, 'test property 3'
          UNION ALL
-         SELECT 'www.fake.image32.jpg', 2, 'test property 3'
+         SELECT 'properties/3/size-original/7f3b28ab-a802-43cc-bca6-14b0c6653832.png', 2, 'test property 3'
          UNION ALL
-         SELECT 'www.fake.image33.jpg', 3, 'test property 3'
+         SELECT 'properties/3/size-original/7f3b28ab-a802-43cc-bca6-14b0c6653833.png', 3, 'test property 3'
      ) AS subquery
          JOIN property pro ON pro.property_title = subquery.property_title
          JOIN picture pic ON pic.picture_url = subquery.picture_url;
