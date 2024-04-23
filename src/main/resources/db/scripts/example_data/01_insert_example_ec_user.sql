9a874ae2bf6c3fed9a9f2f696c74a9c14995c412
Penny Pan <light0104@gmail.com>
2024-04-23 17:43:47 +0800

    將 ecUser 的密碼 example data 加密

--- src/main/resources/db/scripts/example_data/01_insert_example_ec_user.sql
+++ src/main/resources/db/scripts/example_data/01_insert_example_ec_user.sql
@@ -1,3 +1,4 @@
 INSERT INTO ec_user (ec_user_name, ec_user_username, ec_user_email, ec_user_password, ec_user_introduction) VALUES
-('test user 1', 'test username1', 'test email 1', 'test password 1', 'test introduction 1'),
-('test user 2', 'test username2', 'test email 2', 'test password 2', 'test introduction 2');
+('test user 1', 'user1', 'test email 1', '$2a$10$hMEjhKhiRSLOpmw3UT0/7OTLXUkYCLqh7DcoNVdqomsmWnAdtLXIu', 'test introduction 1'),
+('test user 2', 'user2', 'test email 2', '$2a$10$hMEjhKhiRSLOpmw3UT0/7OTLXUkYCLqh7DcoNVdqomsmWnAdtLXIu', 'test introduction 2');
+-- 密碼皆是：123456
