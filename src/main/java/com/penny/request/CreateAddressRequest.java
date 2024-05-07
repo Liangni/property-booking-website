f1d3d2596b10c90ff83e6d16f99d82ce34e9299f
Penny Pan <light0104@gmail.com>
2024-05-07 15:03:44 +0800

    重構 post /addresses

--- src/main/java/com/penny/request/CreateAddressRequest.java
+++ src/main/java/com/penny/request/CreateAddressRequest.java
@@ -1,11 +1,15 @@
 package com.penny.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class CreateAddressRequest {
+    @NotNull
     private Long districtId;
+
+    @NotNull
     private String street;
 }
--- src/main/java/com/penny/service/AddressService.java
+++ src/main/java/com/penny/service/AddressService.java
@@ -4,7 +4,7 @@ import com.penny.dao.AddressVoMapper;
 import com.penny.dao.DistrictVoMapper;
 import com.penny.dao.base.AddressBaseVoMapper;
 import com.penny.dao.base.PropertyBaseVoMapper;
-import com.penny.exception.FieldConflictException;
+import com.penny.exception.RequestValidationException;
 import com.penny.exception.ResourceExistException;
 import com.penny.request.CreateAddressRequest;
 import com.penny.vo.AddressVo;
@@ -25,7 +25,7 @@ public class AddressService {
      * 創建新地址。
      *
      * @param request 地址創建請求
-     * @throws FieldConflictException 如果 districtId 或 street 為 null，或者 district 不是行政劃分層級 3 的行政區
+     * @throws RequestValidationException 如果 districtId 或 street 為 null，或者 district 不是行政劃分層級 3 的行政區
      * @throws ResourceExistException 如果該地址已存在
      */
     public void createAddress(CreateAddressRequest request) {
@@ -33,13 +33,13 @@ public class AddressService {
         String street = request.getStreet();
 
         // 檢查 districtId 和 street 是否為 null
-        if(street == null) throw new FieldConflictException("street is required");
+        if(street == null) throw new RequestValidationException("street is required");
 
-        if (districtId == null) throw new FieldConflictException("districtId is required");
+        if (districtId == null) throw new RequestValidationException("districtId is required");
 
         // 限制 district 只能是行政劃分層級 3 的行政區
         if (districtVoMapper.selectAdminAreaLevelByPrimaryKey(districtId) != 3L) {
-            throw new FieldConflictException("only district at administrative area level 3 is valid. The given district is at level %s".formatted(districtId));
+            throw new RequestValidationException("only district at administrative area level 3 is valid. The given district is at level %s".formatted(districtId));
         }
 
         // 檢查該地址是否已存在
