342cbacdeacdce8107970591ee46734b6fab440f
Penny Pan <light0104@gmail.com>
2024-05-15 14:33:26 +0800

    為 S3Service.generatePreSignedUrl() 方法增加註解

--- src/main/java/com/penny/s3/S3Service.java
+++ src/main/java/com/penny/s3/S3Service.java
@@ -21,11 +21,23 @@ public class S3Service {
 
     private final AmazonS3 amazonS3;
 
+    /**
+     * 生成預簽署 URL，用於指定的存儲桶和文件路徑。
+     *
+     * @param bucketName 存儲桶名稱
+     * @param filePath 文件路徑
+     * @param http HTTP 方法（例如 GET 或 PUT）
+     * @return 預簽署 URL 字串
+     */
     public String generatePreSignedUrl(String bucketName, String filePath, HttpMethod http) {
-
+        // 取得目前時間
         Calendar cal = Calendar.getInstance();
         cal.setTime(new Date());
+
+        // 加上過期時間（以分鐘為單位）
         cal.add(Calendar.MINUTE, EXPIRE_IN_MIN);
+
+        // 生成預簽署 URL 並轉換為字串返回
         return amazonS3.generatePresignedUrl(bucketName,filePath,cal.getTime(),http).toString();
     }
 
